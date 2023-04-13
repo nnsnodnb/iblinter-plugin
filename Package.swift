3c470f91aa536250665629f4b252a78331bdd471
Yuya Oka <nnsnodnb@gmail.com>
2023-04-13 19:09:48 +0900

    Add platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "IBLinterPlugin",
+    platforms: [
+        .macOS(.v10_13)
+    ],
     products: [
         .plugin(
             name: "IBLinterPlugin",
