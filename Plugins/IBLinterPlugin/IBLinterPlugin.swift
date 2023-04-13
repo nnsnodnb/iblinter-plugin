0c3c7d97ed0c3bf4023df13553f8a77f75a21093
Yuya Oka <nnsnodnb@gmail.com>
2023-04-13 18:48:06 +0900

    Revert "buildCommand -> prebuildCommand"
    This reverts commit 87ff8c9e04059319bab77d33768188f565243c5e.

--- Plugins/IBLinterPlugin/IBLinterPlugin.swift
+++ Plugins/IBLinterPlugin/IBLinterPlugin.swift
@@ -11,13 +11,13 @@ import PackagePlugin
 struct IBLinterPlugin: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
         return [
-            .prebuildCommand(
+            .buildCommand(
                 displayName: "Run IBLinter for \(target.name)",
                 executable: try context.tool(named: "iblinter").path,
                 arguments: [
                     "lint"
                 ],
-                outputFilesDirectory: context.pluginWorkDirectory
+                environment: [:]
             )
         ]
     }
@@ -29,14 +29,13 @@ import XcodeProjectPlugin
 extension IBLinterPlugin: XcodeBuildToolPlugin {
     func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
         return [
-            .prebuildCommand(
+            .buildCommand(
                 displayName: "Run IBLinter for \(target.displayName)",
                 executable: try context.tool(named: "iblinter").path,
                 arguments: [
                     "lint"
                 ],
-                environment: [:],
-                outputFilesDirectory: context.pluginWorkDirectory
+                environment: [:]
             )
         ]
     }
